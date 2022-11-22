af2f4948e4dc625028fc8609b1a1e8c8049559a0
Vitoria_Horvath_Miranda <vitoria.h.miranda@gmail.com>
2022-11-22 14:50:10 -0300

    creates plots and updates depth graph script

--- samtools_depth_graph.R
+++ samtools_depth_graph.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(data.table)
 library(dtplyr)
 
-coverage_all <- fread(file = "/home/vitoria/all_samtools_depth.tsv",
+coverage_all <- fread(file = "all_samtools_depth.tsv",
                       sep = "\t", header = FALSE)
 
 bad_variable_names <- str_c("V", 1:17)
@@ -17,9 +17,12 @@ chrom_size <- coverage_all[, .(min(position), max(position)), keyby = chrom]
 
 ggplot(data = chrom_size) +
   geom_histogram(aes(x = V2)) +
-  labs(x = "size")
+  labs(x = "size") 
+
+ggsave("./plots/hist_chrom_size.png", height = 5, width = 10, dpi = 350)
 
 
+# bed_chrom ---------------------------------------------------------------
 strange_chrom <- coverage_all[!chrom %in% c("2L", "2R", "3R", "3L", "X")]
 
 tidy_strange_chrom <- melt(strange_chrom,
@@ -30,28 +33,36 @@ setnames(tidy_strange_chrom, "variable", "population")
 
 head(tidy_strange_chrom)
 
-tidy_strange_chrom %>%
+hist_coverage_badChron <- tidy_strange_chrom %>%
   ggplot() +
   geom_histogram(aes(coverage)) +
   facet_wrap(~ population, nrow = 3) +
-  ggtitle("cromossomos não usuais - no-zoom")
+  ggtitle("cromossomos não usuais")
 
-tidy_strange_chrom %>%
-  ggplot() +
-  geom_histogram(aes(coverage)) +
-  facet_wrap(~ population, nrow = 3) +
-  coord_cartesian(x = c(0, 100)) +
-  ggtitle("cromossomos não usuais - x-zoom")
+ggsave("./plots/hist_coverage_badChron.png",
+       plot = hist_coverage_badChron,
+       height = 7, width = 11, dpi = 400)
 
-tidy_strange_chrom %>%
-  ggplot() +
-  geom_histogram(aes(coverage)) +
-  facet_wrap(~ population, nrow = 3) +
-  coord_cartesian(y = c(0, 1000)) +
+hist_coverageYzoom_badChron <- hist_coverage_badChron +
+  coord_cartesian(y = c(0, 100000)) +
   ggtitle("cromossomos não usuais - y-zoom")
 
-tidy_strange_chrom[, .(max(coverage), mean(coverage)), keyby = population]
-tidy_strange_chrom[, .(max(coverage), mean(coverage)), keyby = chrom] |>
-  setkey()
+ggsave("./plots/hist_coverageYzoom_badChron.png",
+       plot = hist_coverageYzoom_badChron,
+       height = 7, width = 10)
+
+badchrom_pop_summary <- tidy_strange_chrom[, .(max(coverage), mean(coverage)), keyby = population]
+setnames(badchrom_pop_summary, c("V1", "V2"), c("max", "mean"))
+write_csv(badchrom_pop_summary, "./summary/badchrom_pop_summary.csv")
+
+stats_per_chrom <- tidy_strange_chrom[, .(max(coverage), mean(coverage)), keyby = chrom] 
+setnames(stats_per_chrom, c("V1", "V2"), c("max", "mean"))
+setkey(stats_per_chrom, mean)
+
+tail(stats_per_chrom)
+
+
+# 2L 2R 3L 3R X -----------------------------------------------------------
+
 
 
